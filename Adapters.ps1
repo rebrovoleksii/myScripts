2c8083c287170116183dc652358baa178bdd1dc6
arebrov <rebrovoleksii@gmail.com>
2013-08-16 11:11:40 -0400

    Start Adding help for Adapters.ps1 Create dictionary to store xml config values

--- Adapters.ps1
+++ Adapters.ps1
@@ -8,14 +8,37 @@ function runAdapterList([int[]]$listOfAdpaters)
 
 function runAdapterInstance
     {
-	Param($instanceId) 
+     <#
+  .SYNOPSIS
+  Allow to run MT adapter via usm tool
+  .DESCRIPTION
+  This function runs EOP BPA(business processing adapter)
+  using usm.exe tool in case you want run standalone adapter
+  or adapter in interval with unresolved dependencies 
+  Basically yhis function replace next command
+  usm run /instance:xxx /ignoredeps
+  .EXAMPLE
+  runAdapterInstance 1007
+  Description
+  -----------
+  You need to use 4 digit adapter id from MetraControl/DB as parameter.
+  .PARAMETER computername
+  The computer name to query. Just one.
+  .PARAMETER logname
+  The name of a file to write failed computer names to. Defaults to errors.txt.
+  #>
+	Param(
+    [ValidateRange(1000,9999)]
+    [int]
+    $instanceId) 
+  
     Push-Location
     Set-Location $env:mtrmpbin
     usm run /instance:$instanceId /ignoredeps
     Pop-Location
     }
     
-function reverseeAdapterInstanc
+function reverseAdapterInstance
     {
     Param($instanceId) 
     Push-Location
